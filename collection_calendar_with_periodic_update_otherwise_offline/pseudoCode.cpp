03a68254cd26f2570ec93d5b2f752aa0ce3d594d
christianpetri <christianpetri@hotmail.com>
2018-11-12 11:25:41 +0100

    reordered the code

--- collection_calendar_with_periodic_update_otherwise_offline/pseudoCode.cpp
+++ collection_calendar_with_periodic_update_otherwise_offline/pseudoCode.cpp
@@ -1,42 +1,41 @@
 //Libary Stuff
+int colletionDate[][3] = { }; //{Date in Unix, garbageTypeId, circleId}
+String typeName[] =
+{ "Grüngut", "Karton", "Kehricht und Sperrgut", "Metall", "Papier" };
+String circleName[] = { "1", "1a", "1b", "2", "3", "4", "5", "6", "7", "8", "9", };
 
 // The setup() function runs once each time the micro-controller starts 
 void setup(){
-	 
 	if (isWiFiCredentialsSet()){
 		goToSoftAPmode(); //the user has to enter their credentials
 	}
-	 connectToWifi()();
+	 connectToWifi();
 }
 
-// here is where you'd put code that needs to be running all the time.
-void loop(){
+// Add the main program code into the continuous loop() function
+void loop()
+{
 	if (isCalendarStillUpdate()) {
 		digitalClockDisplay();
-		showCurrentEventForCircleId("User_Set_Circle_ID");
+		showCurrentEventForCircleId(«User_Set_Cirle_ID»);
+		delay(1000); //Wait for a second
 	}
 	else {
+		goOnline()
 		getColletionCalendarData();
 		getCurrentUnixTime();
 		goOffline();
-	}
-	delay(1000); //wait for a second
+	} 
 }
+ 
+void connectToWifi(){ ... }
+boolean isWiFiCredentialsSet(){ ... }
+void goToSoftAPmode(){ ... }
+void isCalendarStillUpdate() { ... }
+void digitalClockDisplay() { ... }
+void showCurrentEventForCircleId(int cirleID) { ... }
+void goOnline() { ... }
+void getColletionCalendarData() { ... }
+void getCurrentUnixTime() { ... }
+void goOffline() { ... }
 
-void connectToWifi(){}
-
-boolean isWiFiCredentialsSet(){}
-
-void goToSoftAPmode(){}
-
-void isCalendarStillUpdate() {}
-
-void digitalClockDisplay(){}
-
-void showCurrentEventForCircleId(int CirleID){}
-
-void getColletionCalendarData(){}
-
-void getCurrentUnixTime() {}
-
-void goOffline(){}
